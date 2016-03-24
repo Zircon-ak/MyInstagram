f49a4e54d76ea129db71e583187d490bfa57d70e
Paethai Akarajariyakul <zircon.ak@hotmail.com>
2016-03-24 15:30:54 +0700

    Added Action Sheet with buttons (photo from library, camera and cancel)

---
+++ MyInstagram/ViewControllers/TimelineViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TimelineViewController.swift
+//  MyInstagram
+//
+//  Created by Paethai Akarajariyakul on 3/22/16.
+//  Copyright © 2016 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    var photoTakingHelper : PhotoTakingHelper?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBarController?.delegate = self
+    }
+    
+    func takePhoto(){
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image:UIImage?) in
+            // do nothing yet...
+        }
+        
+        //PhotoTakingHelper(viewController: self.tabBarController!, callback:
+        //    {(image: UIImage?) in
+        
+        //})
+    }
+}
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate{
+    
+    func tabBarController(tabBarController: UITabBarController,
+        shouldSelectViewController viewController: UIViewController) -> Bool{
+            if( viewController is PhotoViewController) {
+                takePhoto()
+                return false
+            }else {
+                return true
+            }
+    }
+}
